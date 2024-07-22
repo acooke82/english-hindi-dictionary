752beed960f182f00be802e068fca84e61562ec4
acooke82 <aecooke82@gmail.com>
2024-07-21 21:02:18 -0700

    added workflow

--- model/noun.py
+++ model/noun.py
@@ -1,10 +1,10 @@
-from model.enums import *
-from model.givenentry import *
-from model.word import *
+#from model.enums import *
+#from model.givenentry import *
+#from model.word import *
 
-class Noun(Word):
-def __init__(self, gender):
-    self.gender = gender
+#class Noun(Word):
+#def __init__(self, gender):
+    #self.gender = gender
 
 
 
